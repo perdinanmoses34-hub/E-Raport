fc9fdb9be3325b8a8f09b7def491687655480300
perdinanmoses34-hub <perdinan.moses34@guru.smp.belajar.id>
2026-09-15 17:34:03 +0700

    ci: add GitHub Pages deployment workflow
    Configure the project for static hosting on GitHub Pages by updating the build base path to relative URLs and adding an automated CI/CD deployment pipeline.

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'e-raport-smp-v1';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon.svg'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
 ];
 
 self.addEventListener('install', (event) => {
